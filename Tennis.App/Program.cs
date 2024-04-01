92f48fb2792b7587478b54f45845d4decb722f90
Jan Strnad <spingee@gmail.com>
2024-04-01 19:49:05 +0200

    Little error handling

--- Tennis.App/Program.cs
+++ Tennis.App/Program.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Tennis.App.Contract;
 using Tennis.Core;
@@ -55,18 +56,17 @@ hostBuilder
                            options.ConfigureTableServiceClient(tableConnString);
                            options.TableName = "PubSubStore";
                        });
-
         }
         else
         {
             builder.AddMemoryGrainStorageAsDefault();
             builder.AddMemoryGrainStorage("PubSubStore");
-            builder.AddMemoryStreams(Constants.QueueStreamName, configurator => configurator.ConfigurePullingAgent(ob => ob.Configure(options =>
-            {
-                options.GetQueueMsgsTimerPeriod = TimeSpan.FromMilliseconds(1);
-            })));
+            builder.AddMemoryStreams(Constants.QueueStreamName,
+                configurator => configurator.ConfigurePullingAgent(ob => ob.Configure(options =>
+                {
+                    options.GetQueueMsgsTimerPeriod = TimeSpan.FromMilliseconds(1);
+                })));
         }
-
     });
 
 
@@ -74,38 +74,56 @@ var app = hostBuilder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet("/match/{name}", async (string name, IGrainFactory factory) =>
-{
-    var matchGrain = factory.GetGrain<IMatchGrain>(name);
-    var result = await matchGrain.GetResult();
-    return new MatchResult(result.IsFinished, result.Sets.Select(SetScore.FromSet).ToList());
-})
-.WithOpenApi(operation =>
-{
-    operation.OperationId = "GetMatch";
-    operation.Summary = "Get the match result";
-    operation.Description = "Get the match result for the given match name";
-    return operation;
-});
+var group = app.MapGroup("/match/")
+               .AddEndpointFilter(async (context, next) =>
+               {
+                   try
+                   {
+                       var result = await next(context);
+                       return result;
+                   }
+                   catch (InvalidOperationException e)
+                   {
+                       context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+                       return e.Message;
+                   }
+               });
 
-app.MapPost("/match/{name}", async (string name, [FromBody]StartMatchRequest request, IGrainFactory factory) =>
-{
-    await factory.GetGrain<IPlayerGrain>(name + "-Player1").Create(name, request.ExperiencePlayer1);
-    await factory.GetGrain<IPlayerGrain>(name + "-Player2").Create(name, request.ExperiencePlayer2);
-    var matchGrain = factory.GetGrain<IMatchGrain>(name);
-    await matchGrain.StartMatch(request);
-})
-.WithOpenApi(operation =>
-{
-    operation.OperationId = "StartMatch";
-    operation.Summary = "Start a new match";
-    operation.Description = "Start a new match with the given players experience levels";
-    return operation;
-});
+group.MapGet("/{name}",
+         async (string name, IGrainFactory factory) =>
+         {
+             var matchGrain = factory.GetGrain<IMatchGrain>(name);
+             var result = await matchGrain.GetResult();
+             return new MatchResult(result.IsFinished, result.Sets.Select(SetScore.FromSet).ToList());
+         })
+     .WithOpenApi(operation =>
+     {
+         operation.OperationId = "GetMatch";
+         operation.Summary = "Get the match result";
+         operation.Description = "Get the match result for the given match name";
+         return operation;
+     });
+
+group.MapPost("/{name}",
+         async (string name, [FromBody] StartMatchRequest request, IGrainFactory factory) =>
+         {
+             await factory.GetGrain<IPlayerGrain>(name + "-Player1").Create(name, request.ExperiencePlayer1);
+             await factory.GetGrain<IPlayerGrain>(name + "-Player2").Create(name, request.ExperiencePlayer2);
+             var matchGrain = factory.GetGrain<IMatchGrain>(name);
+             await matchGrain.StartMatch(request);
+         })
+     .WithOpenApi(operation =>
+     {
+         operation.OperationId = "StartMatch";
+         operation.Summary = "Start a new match";
+         operation.Description = "Start a new match with the given players experience levels";
+         return operation;
+     });
 
 app.Run();
 
 
-
 //needed for tests
-public partial class Program { }
+public partial class Program
+{
+}
